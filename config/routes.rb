7e80919640b2214d61ea1076d75518d97a915ec7
Kelsey Thang <thang.kelsey@gmail.com>
2023-02-12 19:28:15 -0500

    Add cookie settings to backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :properties
   
+  # custom auth routes
+  get '/profile', to: 'users#show'
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # custom method routes
   get 'affordable_properties', to: 'properties#affordable'
   get 'midrange_properties', to: 'properties#midrange'
   get 'luxury_properties', to: 'properties#luxury'
