f90d89ba381c6f2282030190a6a2a393f667344d
Kelsey Thang <thang.kelsey@gmail.com>
2023-03-04 17:19:25 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
-  #namespace :api do
-    resources :images, only: [:create, :destroy]
-    resources :favorites, only: [:index, :create, :destroy]
-    resources :properties
-    
-    # custom auth routes
-    get '/profile', to: 'users#show'
-    post '/signup', to: 'users#create'
-    post '/login', to: 'sessions#create'
-    delete '/logout', to: 'sessions#destroy'
+  resources :images, only: [:create, :destroy]
+  resources :favorites, only: [:index, :create, :destroy]
+  resources :properties
+  
+  # custom auth routes
+  get '/profile', to: 'users#show'
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-    # custom method routes
-    get 'affordable_properties', to: 'properties#affordable'
-    get 'midrange_properties', to: 'properties#midrange'
-    get 'luxury_properties', to: 'properties#luxury'
-  #end
+  # custom method routes
+  get 'affordable_properties', to: 'properties#affordable'
+  get 'midrange_properties', to: 'properties#midrange'
+  get 'luxury_properties', to: 'properties#luxury'
 
-  root 'properties#index'
-  #get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
+  #root 'properties#index'
+  get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
