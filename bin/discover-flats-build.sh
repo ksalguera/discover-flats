b6e552f1e5e4bc9fd2785e1311e88b615d8755ca
Kelsey Thang <thang.kelsey@gmail.com>
2023-03-04 16:09:46 -0500

    Ready app for deployment

--- bin/discover-flats-build.sh
+++ bin/discover-flats-build.sh
@@ -2,9 +2,10 @@
 # exit on error
 set -o errexit
 
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
 
 bundle install
-# bundle exec rake assets:precompile # These lines are commented out because we have an API only app
-# bundle exec rake assets:clean
 bundle exec rake db:migrate
 bundle exec rake db:seed
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  resources :images, only: [:create, :destroy]
-  resources :favorites, only: [:index, :create, :destroy]
-  resources :properties
-  
-  # custom auth routes
-  get '/profile', to: 'users#show'
-  post '/signup', to: 'users#create'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  namespace :api do
+    resources :images, only: [:create, :destroy]
+    resources :favorites, only: [:index, :create, :destroy]
+    resources :properties
+    
+    # custom auth routes
+    get '/profile', to: 'users#show'
+    post '/signup', to: 'users#create'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
 
-  # custom method routes
-  get 'affordable_properties', to: 'properties#affordable'
-  get 'midrange_properties', to: 'properties#midrange'
-  get 'luxury_properties', to: 'properties#luxury'
+    # custom method routes
+    get 'affordable_properties', to: 'properties#affordable'
+    get 'midrange_properties', to: 'properties#midrange'
+    get 'luxury_properties', to: 'properties#luxury'
+  end
 
-  root 'properties#index'
+  #root 'properties#index'
+  get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
