7efef1f3fee7633c5948b76062cda7401b129594
Kelsey Thang <thang.kelsey@gmail.com>
2023-03-23 22:01:01 -0400

    update backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
-  resources :ratings
+  resources :ratings, [:index, :create]
   resources :images, only: [:create, :destroy]
   resources :favorites, only: [:index, :create, :destroy]
   resources :properties
   
-  # custom auth routes
   get '/profile', to: 'users#show'
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  # custom method routes
-  get 'affordable_properties', to: 'properties#affordable'
-  get 'midrange_properties', to: 'properties#midrange'
-  get 'luxury_properties', to: 'properties#luxury'
-
-  #root 'properties#index'
   get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
