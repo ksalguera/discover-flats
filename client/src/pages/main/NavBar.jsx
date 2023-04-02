bffff70bcdcb992ff06b8cdadd17f5a5a51b496e
Kelsey Thang <thang.kelsey@gmail.com>
2023-04-02 17:16:50 -0400

    update navbar

--- client/src/pages/main/NavBar.jsx
+++ client/src/pages/main/NavBar.jsx
@@ -10,6 +10,8 @@ const NavBar = () => {
   const { setFavorites } = useContext(FavoriteContext);
   const navigate = useNavigate();
   
+  const handleLogin = () => !user ? navigate('/login') : handleLogOut;
+  
   const handleLogOut = async () => {
     const res = await fetch('/logout', { method: 'DELETE' });
     if (res.ok) { 
@@ -26,7 +28,7 @@ const NavBar = () => {
         { user && <Link component={RouterLink} underline='none' sx={navbarStyle} to='/favorites'>Favorties</Link> }
         { user && <Link component={RouterLink} underline='none' sx={navbarStyle} to='/reviews'>Reviews</Link> }
         { user && <Link component={RouterLink} underline='none' sx={navbarStyle} to='/profile'>Profile</Link> }
-        <Button variant='outlined' size='small' onClick={!user ? navigate('/login') : handleLogOut} >{ !user ? 'Login' : 'Log Out'}</Button>
+        <Button variant='outlined' size='small' onClick={handleLogin} >{ !user ? 'Login' : 'Log Out'}</Button>
       </Stack>
     </>
   )
